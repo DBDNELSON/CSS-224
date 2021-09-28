9eb5fdc478f683120db41d5d8da585e71974d438
DBDNELSON <91555286+DBDNELSON@users.noreply.github.com>
2021-09-28 10:54:41 -0500

    w1 in class
    Is this the end?

---
+++ w1 in-class.py
@@ -0,0 +1,15 @@
+print('hello world')
+print("hello world")
+
+
+print(1+2)
+print(7*6)
+
+
+print()
+
+
+print("the end")
+print("the end" , "or is it?", "stay tuned to learn more about python", 3)
+
+
